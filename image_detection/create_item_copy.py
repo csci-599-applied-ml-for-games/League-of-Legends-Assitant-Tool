1195728eb1fd264d06d019922592ce96859983d7
alvinzhou66 <43176447+alvinzhou66@users.noreply.github.com>
2020-02-10 21:08:52 -0800

    Add files via upload

---
+++ image_detection/create_item_copy.py
@@ -0,0 +1,48 @@
+#%%
+import os
+import os.path
+import shutil
+def traversefile(workdir):
+    for i in range(0,280):
+        count = 1
+        for file in os.listdir(workdir):
+            #拷贝的是文件，如是目录则需要在遍历然后拷贝
+            srcFile = os.path.join(workdir,file)
+            output_path = 'lol_data_2/training/{}'.format(file.replace('.png',''))
+            output_path = output_path.replace('1','')
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+            filename = file.replace('item', '{}'.format(i))
+            targetFile = os.path.join(output_path, filename)
+            shutil.copyfile(srcFile,targetFile)
+
+            count += 1
+    return count
+
+traverse = traversefile(workdir='lol_data_2/images')
+print(traverse)
+
+
+
+# %%
+def traversefile1(workdir):
+    for i in range(280,360):
+        count = 1
+        for file in os.listdir(workdir):
+            #拷贝的是文件，如是目录则需要在遍历然后拷贝
+            srcFile = os.path.join(workdir,file)
+            output_path = 'lol_data_2/valid/{}'.format(file.replace('.png',''))
+            output_path = output_path.replace('1','')
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+            filename = file.replace('item', '{}'.format(i))
+            targetFile = os.path.join(output_path, filename)
+            shutil.copyfile(srcFile,targetFile)
+
+            count += 1
+    return count
+
+traverse1 = traversefile1(workdir='lol_data_2/images')
+print(traverse1)
+
+# %%
